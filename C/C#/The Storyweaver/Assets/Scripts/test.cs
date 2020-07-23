ee34a26afb33b61ea3e8308b0666481882085fc2
Noxellar-remote-Dell-Inspiron-7380 <vhle30@gmail.com>
2020-07-23 13:01:06 +1000

    ajfkj;ldkfs

---
+++ C/C#/The Storyweaver/Assets/Scripts/test.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+	string OnClick()
+	{
+		return "alksdjf;la";
+		Debug.Log("asdfasfl");
+	}
+}
