35889aff88a4786d5d9eb126477831edcc8d8acc
Noxellar-remote-Dell-Inspiron-7380 <vhle30@gmail.com>
2020-07-24 18:47:23 +1000

    - Google FooBar     - Test 1         - Rewriting the whole thing

--- Python/Google FooBar/Test 1/solution.py
+++ Python/Google FooBar/Test 1/solution.py
@@ -1,14 +1,6 @@
 def solution(s):
     for length in range(1, len(s)):
-        pattern = s[0:length + 1]
-
-        for count in range(length, len(s), length):
-            iter = s[(count * length):((count + 1) * length + 1)]
-            print((count * length), ((count + 1) * length + 1)))
-            if pattern == iter:
-                print(pattern)
-            else:
-                break
+        list = [i for i in range(1, len(s) + 1) if len(s) % i == 0]
 
 
 solution("abccbaabccbaabccba")
