740ee4d18172a70e780d55761a59b89a27df74a7
Noxellar-local-EX58 <vhle30@gmail.com>
2020-07-25 14:36:31 +1000

    - Google FooBar     - Finished Test 1

--- Python/Google FooBar/Test 1/solution.py
+++ Python/Google FooBar/Test 1/solution.py
@@ -1,6 +1,30 @@
 def solution(s):
-    for length in range(1, len(s)):
-        list = [i for i in range(1, len(s) + 1) if len(s) % i == 0]
+    # Generate a list of factors from length of string
+    list = [factor for factor in range(1, len(s) + 1) if len(s) % factor == 0]
 
+    for factor in list:
+        # Possible pattern configuration
+        pattern = s[0:factor]
+        # Maximum number of iterations possible without going
+        # past string length
+        max_iter = int(len(s) / factor)
 
-solution("abccbaabccbaabccba")
+        # Loop through the string and compare with
+        # possible configuration
+        for count in range(1, max_iter):
+            # Splicing from factor * count
+            # Don't want to compare configuration with itself
+            iter_text = s[factor * count:factor * (count + 1)]
+
+            if pattern != iter_text:
+                # Exit loop immediately if match doesn't work
+                break
+        else:
+            # If all iterations are the same, return max number
+            # of cake slices
+            return max_iter
+    else:
+        # Code is set not to compare configuration with itself
+        # So if the M&Ms are "abcdefg" then it won't work
+        # Just return whole string back
+        return s
--- Python/test.py
+++ Python/test.py
@@ -1,11 +1,3 @@
-class A(object):
-    static = "alsdjflkajds;lf"
+list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-
-class B(A):
-    def method(self):
-        print(self.static)
-
-
-b = B()
-b.method()
+print(list[0:1 + 1])
