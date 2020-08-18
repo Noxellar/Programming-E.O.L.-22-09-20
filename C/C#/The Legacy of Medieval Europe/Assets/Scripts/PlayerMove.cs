7033d5ddb2f14e05a488f2c0137c17626cb4527c
Noxellar Lee (Inspiron-7380) <vhle30@gmail.com>
2020-08-18 17:22:18 +1000

    Completed script for moving the player in assignment Added TODO.md for assignment Moved TODO.md for The Storyweaver Removed C# folder from .gitignore

---
+++ C/C#/The Legacy of Medieval Europe/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+	// Player rigidbody variable declaration
+	Rigidbody playerRigidbody;
+
+	// Player Input Action variable declaration
+	PlayerInputActions inputAction;
+
+	// Movement Input variable declaration
+	Vector2 movementInput;
+
+	// Movement Speed variable declaration
+	public float movementSpeed;
+
+	void Awake()
+	{
+		// Player rigidbody variable assignment
+		playerRigidbody = GetComponent<Rigidbody>();
+
+		// Player input action variable assignment to input action maps called "Player Input Actions"
+		inputAction = new PlayerInputActions();
+
+		// .Player is a action map and .Move is an action
+		inputAction.Player.Move.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
+
+		// Movement Speed variable assignment in m/s
+		movementSpeed = 3f;
+	}
+
+	void Update()
+	{
+		// Player x and y velocities. Time.deltaTime is not included because
+		// movementSpeed is in already velocity: m/s
+		Vector3 playerVelocityX = (transform.right * movementInput.x) * movementSpeed;
+		Vector3 playerVelocityY = (transform.forward * movementInput.y) * movementSpeed;
+
+		playerRigidbody.velocity = playerVelocityX + playerVelocityY;
+	}
+
+	void OnEnable()
+	{
+		inputAction.Enable();
+	}
+
+	void OnDisable()
+	{
+		inputAction.Disable();
+	}
+}
