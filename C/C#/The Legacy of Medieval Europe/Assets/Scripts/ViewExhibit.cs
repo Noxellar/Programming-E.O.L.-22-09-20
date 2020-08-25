9f219a8e71c038907e3c302b33d6e13b44e2d536
Noxellar Lee (Inspiron-7380) <vhle30@gmail.com>
2020-08-25 14:48:48 +1000

    Added canvases. Now need to put in information

--- C/C#/The Legacy of Medieval Europe/Assets/Scripts/ViewExhibit.cs
+++ C/C#/The Legacy of Medieval Europe/Assets/Scripts/ViewExhibit.cs
@@ -7,10 +7,16 @@ public class ViewExhibit : MonoBehaviour
 	public void Open()
 	{
 		gameObject.transform.Find("Information").gameObject.SetActive(true);
+
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
 	}
 
 	public void Close()
 	{
 		gameObject.transform.Find("Information").gameObject.SetActive(false);
+
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
 	}
 }
